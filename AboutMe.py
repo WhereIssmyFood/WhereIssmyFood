5483f30123029dda7e507117c355eb15d16f6a88
WhereIssmyFood <144620226+WhereIssmyFood@users.noreply.github.com>
2023-09-10 20:23:37 +0200

    Create AboutMe.py
    Information about me, Food!

---
+++ AboutMe.py
@@ -0,0 +1,44 @@
+Username = "WhereIssMyFood"
+ShareInfo = False
+
+if Username == "WhereIsMyFood":
+  print("That's my name!")				
+elif Username == "WhereIssMyFood":
+	print("That's my Name, but with an Additional s lol")
+	ShareInfo = True
+	return ShareInfo
+else:
+	print("What is my name..?")
+	input(">> ")
+
+UserInterestDictionary = {
+	# Online
+	"Coding":"Python, Lua, C++, C#, Javascript, Java",
+	"Discord":"Food#9172",
+	"Frameworks":"Pycord, Pygame",
+	"Developing":"Games, Websites",
+	# Personal
+	"AboutMe":"I like Python the most, And I like cooking",
+	"Cooking":True,
+	"Age":14
+}
+
+WhatAmILearningList = [
+	["Intermediate"]["Python", "Lua", "HTML and CSS", "Javascript"]
+	["Beginner"]["Java", "C++ and C#"]
+	["In the Future..."]["I will learn SQL because Why not?"]
+]
+
+
+Contact = """
+[REDACTED] for Business
+ninendoswitchiscool@gmail.com for Random stuff.
+"""
+
+Collaborations = f"I just wanna learn Coding, so if you have time to kill or wanna have fun, maybe {Contact} me!"
+
+if ShareInfo == True:
+	print(UserInterestDictionary, WhatAmILearningList, Contact, Collaborations)
+else:
+	print("go away")
+	
